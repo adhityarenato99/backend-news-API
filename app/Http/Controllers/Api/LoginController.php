d67851abbb7197d3cf15891d5de4e86af76fb409
DESKTOP-M509QTO\HP <adhitya.dwijayantho779@gmail.com>
2023-02-23 23:03:48 +0700

    first commit

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        // if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // get credentials from request
+        $credentials = $request->only('email', 'password');
+
+        // if auth failed
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda salah'
+            ], 401);
+        }
+
+        // $user = auth()->user();
+        // $id = $user->id;
+        // var_dump($id);die;
+
+        // if auth success
+        return response()->json([
+            'success' => true,
+            'user' => auth()->user(),
+            'token' => $token
+        ], 200);
+    }
+}
